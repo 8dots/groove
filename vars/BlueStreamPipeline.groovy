ee59583f1a0894845715f6ef7a54ce1f75c686d8
8dots <bluedevops@gmail.com>
2018-12-03 16:53:07 +0200

    without podTemplate

--- vars/BlueStreamPipeline.groovy
+++ vars/BlueStreamPipeline.groovy
@@ -9,7 +9,6 @@ def call() {
       stage('Unit-Testing') {
         container('jenkins-build-slave') {
           withCredentials([string(credentialsId: 'ACRUSER', variable: 'ACRUSER'), string(credentialsId: 'ACRPASS', variable: 'ACRPASS'), string(credentialsId: 'BS_RMQ_SERVER', variable: 'RMQ_SERVER'), string(credentialsId: 'BS_DB_SERVER', variable: 'DB_SERVER')]) {    
-            checkout scm
             GitShortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
             sh "docker login bluehub.azurecr.io -u $ACRUSER -p $ACRPASS"
             sh "docker build -t bluehub.azurecr.io/${p.repoName}:${GitShortCommit} ."
