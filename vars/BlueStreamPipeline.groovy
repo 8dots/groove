38e2010dba21a342930ba98936790aa239bde2c4
8dots <bluedevops@gmail.com>
2018-12-03 13:49:12 +0200

    fix vars

--- vars/BlueStreamPipeline.groovy
+++ vars/BlueStreamPipeline.groovy
@@ -5,6 +5,7 @@ def call() {
   node {
     stage('Checkout') {
       checkout scm
+      GitShortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
     }
     def p = pipelineCfg()
 
@@ -13,8 +14,8 @@ def call() {
         container('jenkins-build-slave') {
           withCredentials([string(credentialsId: 'ACRUSER', variable: 'ACRUSER'), string(credentialsId: 'ACRPASS', variable: 'ACRPASS'), string(credentialsId: 'BS_RMQ_SERVER', variable: 'RMQ_SERVER'), string(credentialsId: 'BS_DB_SERVER', variable: 'DB_SERVER')]) {    
             sh "docker login bluehub.azurecr.io -u $ACRUSER -p $ACRPASS"
-            sh "docker build -t bluehub.azurecr.io/${repoName}:${GIT_SHORT_COMMIT} ."
-            sh "docker run --env DB_SERVER=$DB_SERVER --env RMQ_HOST=$RMQ_SERVER bluehub.azurecr.io/${repoName}:${GIT_SHORT_COMMIT} npm test"
+            sh "docker build -t bluehub.azurecr.io/${p.repoName}:${GitShortCommit} ."
+            sh "docker run --env DB_SERVER=$DB_SERVER --env RMQ_HOST=$RMQ_SERVER bluehub.azurecr.io/${p.repoName}:${GitShortCommit} npm test"
           }
         }
       }
