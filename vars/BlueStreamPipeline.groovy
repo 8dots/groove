88f0169a95a93eb47df98638c7211a9d1af0f035
8dots <bluedevops@gmail.com>
2019-01-07 10:37:20 +0200

    test with docker login

--- vars/BlueStreamPipeline.groovy
+++ vars/BlueStreamPipeline.groovy
@@ -11,19 +11,18 @@ def call() {
     if (p.runTests == true) {
       stage('Unit-Testing') {
         container('jenkins-build-slave') {
-          // withCredentials([
-            sh "echo debug"
-            //   string(credentialsId: 'ACRUSER', variable: 'ACRUSER'), 
-            //   string(credentialsId: 'ACRPASS', variable: 'ACRPASS'), 
-            //   string(credentialsId: 'ACR_ENDPOINT', variable: 'ACR_ENDPOINT'), 
-            //   string(credentialsId: 'BS_CONFIG', variable: 'BS_CONFIG')
-            //   ]) {    
-            // checkout scm
-            // GitShortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
-            // sh "docker login $ACR_ENDPOINT -u $ACRUSER -p $ACRPASS"
-            // sh "docker build -t $ACR_ENDPOINT/${p.repoName}:${GitShortCommit} ."
-            // sh "docker run --env-file $BS_CONFIG $ACR_ENDPOINT/${p.repoName}:${GitShortCommit} npm test"
-          // }
+          withCredentials([
+              string(credentialsId: 'ACRUSER', variable: 'ACRUSER'), 
+              string(credentialsId: 'ACRPASS', variable: 'ACRPASS'), 
+              string(credentialsId: 'ACR_ENDPOINT', variable: 'ACR_ENDPOINT'), 
+              string(credentialsId: 'BS_CONFIG', variable: 'BS_CONFIG')
+              ]) {    
+            checkout scm
+            GitShortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
+            sh "docker login $ACR_ENDPOINT -u $ACRUSER -p $ACRPASS"
+            sh "docker build -t $ACR_ENDPOINT/${p.repoName}:${GitShortCommit} ."
+            sh "docker run --env-file $BS_CONFIG $ACR_ENDPOINT/${p.repoName}:${GitShortCommit} npm test"
+          }
         }
       }
     }
